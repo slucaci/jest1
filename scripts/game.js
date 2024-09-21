2f2872b3dc1dec4a6e385a405d0588478727c52f
slucaci <serjlucaci@gmail.com>
2024-09-21 10:00:22 +0100

    Using Game State

--- scripts/game.js
+++ scripts/game.js
@@ -3,6 +3,7 @@ let game = {
   playerMoves: [],
   score: 0,
   choices: ["button1", "button2", "button3", "button4"],
+  turnNumber: 0,
 };
 
 function newGame() {
@@ -16,7 +17,7 @@ function newGame() {
 function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
-  // showTurns();
+  showTurns();
 }
 
 function lightsOn(circ) {
@@ -30,4 +31,15 @@ function showScore() {
   document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn };
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(() => {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
