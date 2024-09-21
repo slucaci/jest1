981dfea3ffe7392e05548b47b6598577d3b47fdd
slucaci <serjlucaci@gmail.com>
2024-09-21 10:13:21 +0100

    Testing the DOM

--- scripts/game.js
+++ scripts/game.js
@@ -2,14 +2,26 @@ let game = {
   currentGame: [],
   playerMoves: [],
   score: 0,
-  choices: ["button1", "button2", "button3", "button4"],
   turnNumber: 0,
+  choices: ["button1", "button2", "button3", "button4"],
 };
 
 function newGame() {
   game.currentGame = [];
   game.playerMoves = [];
   game.score = 0;
+
+  for (let circle of document.getElementsByClassName("circle")) {
+    if (circle.getAttribute("data-listener") !== "true") {
+      circle.addEventListener("click", (e) => {
+        let move = e.target.getAttribute("id");
+        lightsOn(move);
+        game.playerMoves.push(move);
+        playerTurn();
+      });
+      circle.setAttribute("data-listener", "true");
+    }
+  }
   showScore();
   addTurn();
 }
@@ -18,6 +30,17 @@ function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
   showTurns();
+}
+
+function showTurns() {
+  game.turnNumber = 0;
+  let turns = setInterval(function () {
+    lightsOn(game.currentGame[game.turnNumber]);
+    game.turnNumber++;
+    if (game.turnNumber >= game.currentGame.length) {
+      clearInterval(turns);
+    }
+  }, 800);
 }
 
 function lightsOn(circ) {
@@ -29,17 +52,6 @@ function lightsOn(circ) {
 
 function showScore() {
   document.getElementById("score").innerText = game.score;
-}
-
-function showTurns() {
-  game.turnNumber = 0;
-  let turns = setInterval(() => {
-    lightsOn(game.currentGame[game.turnNumber]);
-    game.turnNumber++;
-    if (game.turnNumber >= game.currentGame.length) {
-      clearInterval(turns);
-    }
-  }, 800);
 }
 
 module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
