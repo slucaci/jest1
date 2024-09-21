8e044b4c7b530bdb9fe0934cae9bfbf0376a275a
slucaci <serjlucaci@gmail.com>
2024-09-21 09:51:15 +0100

    More Advanced Tests - Part 2

--- scripts/game.js
+++ scripts/game.js
@@ -16,10 +16,18 @@ function newGame() {
 function addTurn() {
   game.playerMoves = [];
   game.currentGame.push(game.choices[Math.floor(Math.random() * 4)]);
+  // showTurns();
+}
+
+function lightsOn(circ) {
+  document.getElementById(circ).classList.add("light");
+  setTimeout(function () {
+    document.getElementById(circ).classList.remove("light");
+  }, 400);
 }
 
 function showScore() {
   document.getElementById("score").innerText = game.score;
 }
 
-module.exports = { game, newGame, showScore, addTurn };
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
